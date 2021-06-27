6e3fba36b1d8153ba158f1106feafd12c381be36
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-06-27 12:13:55 +0530

    Update MyController.java
    na

--- Redis-Sample/src/main/java/dummy/controller/MyController.java
+++ Redis-Sample/src/main/java/dummy/controller/MyController.java
@@ -2,7 +2,6 @@ package dummy.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dummy.model.Account;
@@ -18,8 +18,21 @@ import dummy.model.Transaction;
 import dummy.repo.CustomerRepository;
 import dummy.repo.RedisBatchRepository;
 import dummy.repo.TransactionRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
+/**
+ *  This class is an API exposed to CRUD operation using Redis Server.
+ *  You can add more API as you want  with respect to Swagger OpenAPI 3.0 with security
+ * @author Sayantan
+ * @since JDK 1.0 
+ * @version 1.0
+ * @see RedisBatchRepository
+ * @see CustomerRepository
+ * @see TransactionRepository
+ */
 @RestController
+@RequestMapping("/api")
 public class MyController {
 	 
 	@Autowired
@@ -30,7 +43,11 @@ public class MyController {
 	
 	@Autowired
     TransactionRepository transrepository;
-
+	/**
+	 * To save as a <code>Customer</code> cache with Redis.  
+	 * @param customer needs to be JSON body
+	 * @return {@link Customer} 
+	 */
     @PostMapping(value = "/addcustomer",
 					produces = MediaType.APPLICATION_JSON_VALUE,
 					consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -38,84 +55,117 @@ public class MyController {
     public Customer add(@RequestBody Customer customer) {
         return repository.save(customer);
     }
-
+    /**
+     * To delete a <code>Customer</code> cache with it's identifier.
+     * @param id identifier of a Customer
+     * @return {@link Customer}
+     */
+    @ApiOperation(value = "Search Customer by customerId", produces = "application/json")
     @GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public Customer findById(@PathVariable("id") Long id) {
-        Optional<Customer> optCustomer = repository.findById(id);
-        return optCustomer.orElse(null);
+    public Customer findById(@ApiParam(name = "customerId",
+            value = "The Id of the customer to be viewed",
+            required = true)@PathVariable("id") String id) {
+    	return repository.findById(id).get(); 
     }
-    
+    /**
+     * To get a <code>Transaction</code> cache with it's identifier
+     * @param id identifier of a Transaction 
+     * @return {@link Transaction}
+     */
+    @ApiOperation(value = "Search transaction by transactionId", produces = "application/json")
     @GetMapping(value = "/{id}",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public Transaction findByIdTrans(Long id) {
-        Optional<Transaction> optTransaction = transrepository.findById(id);
-        return optTransaction.orElse(null);
-    }
+    public Transaction findByIdTrans(@ApiParam(name = "transactionId",
+            value = "The Id of the transaction to be viewed",
+            required = true)@PathVariable("id") String id) {
+    	return transrepository.findById(id).get();
 
+    }
+    /**
+     * To get a <code>List</code> of <code>Transaction</code> from <code>accountId</code>
+     * @param accountId identifier of an account.
+     * @return <code>List</code> - <code>Transaction</code>
+     */
+    @ApiOperation(value = "Search from Account by accountId", produces = "application/json")
     @GetMapping(value = "/from/{accountId}",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public List<Transaction> findByFromAccountId(@PathVariable("accountId") Long accountId) {
-        return transrepository.findByFromAccountId(accountId);
+    public List<Transaction> findByFromAccountId(
+    		@ApiParam(name = "studentId",
+            value = "The Id from the account to be viewed",
+            required = true)@PathVariable("accountId") String accountId) {
+    	return transrepository.findByFromAccountId(accountId);
     }
-
-    @GetMapping(value = "/to/{accountId}",
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			headers = "Accept=application/json")
-    public List<Transaction> findByToAccountId(@PathVariable("accountId") Long accountId) {
-        return transrepository.findByToAccountId(accountId);
+    /**
+     * To get a <code>List</code> of <code>Transaction</code> to <code>accountId</code>
+     * @param accountId identifier of an account.
+     * @return <code>List</code> - <code>Transaction</code>
+     */
+    @ApiOperation(value = "Search Customer by customerId", produces = "application/json")
+    @GetMapping(value = "/to/{accountId}",produces = MediaType.APPLICATION_JSON_VALUE,headers = "Accept=application/json")
+    public List<Transaction> findByToAccountId(@ApiParam(name = "transactionId",value = "The Id to the Transaction to be viewed",required = true)
+    @PathVariable("accountId") String accountId) {
+    	return transrepository.findByToAccountId(accountId);
     }
-    
+    /**
+     * To insert all Customers with batch processing
+     */
     @GetMapping(value = "/insertAllCust",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
     public void insertAllCust() {
     	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account(1367, "54000", 54));
-    	acclist.add( new Account(1368, "54000", 54));
+    	acclist.add(new Account("1367", "54001", 54)); 
+    	acclist.add( new Account("1368", "54000", 54));
     	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer(1, "34", "abc", acclist));
-    	custlist.add(new Customer(2, "34", "abc", acclist));
+    	custlist.add(new Customer("1", "34", "abc", acclist));
+    	custlist.add(new Customer("2", "34", "abc", acclist));
 		batchrepository.insertAll(custlist, "customer");
 	}
-    
+    /**
+     * To update All customers with batch processing
+     */
     @GetMapping(value = "/updateAllCust",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
     public void updateAllCust() { 
     	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account(1367, "54000", 54));
-    	acclist.add( new Account(1368, "54000", 54));
+    	acclist.add(new Account("1367", "54000", 54));
+    	acclist.add( new Account("1368", "54000", 54));
     	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer(1, "34", "abc", acclist));
-    	custlist.add(new Customer(2, "34", "abc", acclist));
+    	custlist.add(new Customer("1", "34", "abc", acclist));
+    	custlist.add(new Customer("2", "34", "abc", acclist));
     	batchrepository.updateAllCustomers(custlist, "sayantan");
 	}
-    
+    /**
+     * To update All accounts with batch processing
+     */
     @GetMapping(value = "/updateAllAcc",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
     public void updateAllAccount() { 
     	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account(1367, "54000", 54));
-    	acclist.add( new Account(1368, "54000", 54));
+    	acclist.add(new Account("1367", "54000", 54));
+    	acclist.add( new Account("1368", "54000", 54));
     	batchrepository.updateAllAccounts(acclist, 58);
 	}
-    
+    /**
+     * To delete All customers with batch processing
+     */
     @GetMapping(value = "/deleteAllCust",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
     public void deleteAllCust() { 
     	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account(1367, "54000", 54));
-    	acclist.add( new Account(1368, "54000", 54));
+    	acclist.add(new Account("1367", "54000", 54));
+    	acclist.add( new Account("1368", "54000", 54));
     	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer(1, "34", "abc", acclist));
-    	custlist.add(new Customer(2, "34", "abc", acclist));
+    	custlist.add(new Customer("1", "34", "abc", acclist));
+    	custlist.add(new Customer("2", "34", "abc", acclist));
     	batchrepository.deleteAll(custlist, "sayantan");
 	}
 }
