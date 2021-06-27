9eb3720dbb404981c61b0da88cafb317493fdf3d
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-06-27 12:17:04 +0530

    Update Transaction.java
    na

--- Redis-Sample/src/main/java/dummy/model/Transaction.java
+++ Redis-Sample/src/main/java/dummy/model/Transaction.java
@@ -7,10 +7,17 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 import org.springframework.stereotype.Component;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 
+ * @author Sayantan
+ *
+ */
 @Data
 @AllArgsConstructor
 @RedisHash("transaction")
@@ -19,11 +26,20 @@ import lombok.NoArgsConstructor;
 public class Transaction {
 
 	@Id
-    private Long id; 
+	@SerializedName("id")
+    private String id; 
+	
+	@SerializedName("amount")
     private int amount;
+	
+	@SerializedName("date")
     private Date date;
+	
     @Indexed
+    @SerializedName("fromAccountId")
     private long fromAccountId;
+    
     @Indexed
+    @SerializedName("toAccountId")
     private long toAccountId;
 }
