6cb11cc2d8c7cb6f04a96ff38ee4d3fa82896a87
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-06-30 12:52:33 +0530

    Update RedisSampleApplication.java
    na

--- Redis-Sample/src/main/java/dummy/RedisSampleApplication.java
+++ Redis-Sample/src/main/java/dummy/RedisSampleApplication.java
@@ -3,6 +3,11 @@ package dummy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Sayantan
+ *http://localhost:8080/swagger-ui/index.html?configUrl=/api-docs/swagger-config
+ */
 @SpringBootApplication 
 public class RedisSampleApplication { 
 
