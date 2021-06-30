bec9fe4c988f8080d91b3a36e7d67fac63cb3d1d
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-06-30 12:44:37 +0530

    Update MyController.java
    na

--- Redis-Sample/src/main/java/dummy/controller/MyController.java
+++ Redis-Sample/src/main/java/dummy/controller/MyController.java
@@ -1,15 +1,14 @@
 package dummy.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dummy.model.Account;
@@ -19,24 +18,28 @@ import dummy.repo.CustomerRepository;
 import dummy.repo.RedisBatchRepository;
 import dummy.repo.TransactionRepository;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 /**
  *  This class is an API exposed to CRUD operation using Redis Server.
  *  You can add more API as you want  with respect to Swagger OpenAPI 3.0 with security
- * @author Sayantan 
+ * @author Sayantan
  * @since JDK 1.0 
  * @version 1.0
  * @see RedisBatchRepository
  * @see CustomerRepository
  * @see TransactionRepository
  */
-@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080") }, info = @Info(title = "Sample Spring Boot API", version = "v2", description = "A demo project using Spring Boot with Swagger-UI enabled", license = @License(name = "MIT License", url = "www.abc.com"), contact = @Contact(url = "www.abc.com", name = "Sayantan Chatterjee")))
+@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080") }, info = @Info(title = "Sample Spring Boot API", version = "v1", description = "A demo project using Spring Boot with Swagger-UI enabled", license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"), contact = @Contact(url = "https://www.linkedin.com/in/bchen04/", name = "Sayantan Chatterjee")))
 @RestController
 @RequestMapping("/api")
 public class MyController {
@@ -54,6 +57,8 @@ public class MyController {
 	 * @param customer needs to be JSON body
 	 * @return {@link Customer} 
 	 */
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
     @PostMapping(value = "/addcustomer",
 					produces = MediaType.APPLICATION_JSON_VALUE,
 					consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -66,14 +71,13 @@ public class MyController {
      * @param id identifier of a Customer
      * @return {@link Customer}
      */
-    @ApiOperation(value = "Search Customer by customerId", produces = "application/json")
-    @GetMapping(value = "/{id}",
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+    @GetMapping(value = "customer",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public Customer findById(@ApiParam(name = "customerId",
-            value = "The Id of the customer to be viewed",
-            required = true)@PathVariable("id") String id) {
+    public Customer findById(@RequestParam("custid") String id) {
     	return repository.findById(id).get(); 
     }
     /**
@@ -82,12 +86,10 @@ public class MyController {
      * @return {@link Transaction}
      */
     @ApiOperation(value = "Search transaction by transactionId", produces = "application/json")
-    @GetMapping(value = "/{id}",
+    @GetMapping(value = "transaction",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public Transaction findByIdTrans(@ApiParam(name = "transactionId",
-            value = "The Id of the transaction to be viewed",
-            required = true)@PathVariable("id") String id) {
+    public Transaction findByIdTrans(@RequestParam("transid") String id) {
     	return transrepository.findById(id).get();
 
     }
@@ -96,14 +98,12 @@ public class MyController {
      * @param accountId identifier of an account.
      * @return <code>List</code> - <code>Transaction</code>
      */
-    @ApiOperation(value = "Search from Account by accountId", produces = "application/json")
-    @GetMapping(value = "/from/{accountId}",
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+       @GetMapping(value = "/fromaccount",
 			produces = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public List<Transaction> findByFromAccountId(
-    		@ApiParam(name = "studentId",
-            value = "The Id from the account to be viewed",
-            required = true)@PathVariable("accountId") String accountId) {
+    public List<Transaction> findByFromAccountId(@RequestParam("fromaccountId") String accountId) {
     	return transrepository.findByFromAccountId(accountId);
     }
     /**
@@ -111,67 +111,56 @@ public class MyController {
      * @param accountId identifier of an account.
      * @return <code>List</code> - <code>Transaction</code>
      */
-    @ApiOperation(value = "Search Customer by customerId", produces = "application/json")
-    @GetMapping(value = "/to/{accountId}",produces = MediaType.APPLICATION_JSON_VALUE,headers = "Accept=application/json")
-    public List<Transaction> findByToAccountId(@ApiParam(name = "transactionId",value = "The Id to the Transaction to be viewed",required = true)
-    @PathVariable("accountId") String accountId) {
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+       @GetMapping(value = "/toaccount",produces = MediaType.APPLICATION_JSON_VALUE,headers = "Accept=application/json")
+    public List<Transaction> findByToAccountId(@RequestParam("toaccountId") String accountId) {
     	return transrepository.findByToAccountId(accountId);
     }
     /**
      * To insert all Customers with batch processing
      */
-    @GetMapping(value = "/insertAllCust",
-			produces = MediaType.APPLICATION_JSON_VALUE,
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+    @PostMapping(value = "/insertAllCust",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public void insertAllCust() {
-    	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account("1367", "54001", 54)); 
-    	acclist.add( new Account("1368", "54000", 54));
-    	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer("1", "34", "abc", acclist));
-    	custlist.add(new Customer("2", "34", "abc", acclist));
-		batchrepository.insertAll(custlist, "customer");
+    public void insertAllCust(@RequestBody List<Customer> custList) {
+		
+		  batchrepository.insertAll(custList, "customer");
 	}
     /**
      * To update All customers with batch processing
      */
-    @GetMapping(value = "/updateAllCust",
-			produces = MediaType.APPLICATION_JSON_VALUE,
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+   @PostMapping(value = "/updateAllCust",
+			consumes  = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public void updateAllCust() { 
-    	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account("1367", "54000", 54));
-    	acclist.add( new Account("1368", "54000", 54));
-    	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer("1", "34", "abc", acclist));
-    	custlist.add(new Customer("2", "34", "abc", acclist));
-    	batchrepository.updateAllCustomers(custlist, "sayantan");
+    public void updateAllCust(@RequestBody List<Customer> custList) { 
+		  batchrepository.updateAllCustomers(custList, "sayantan");
 	}
     /**
      * To update All accounts with batch processing
      */
-    @GetMapping(value = "/updateAllAcc",
-			produces = MediaType.APPLICATION_JSON_VALUE,
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
+    @PostMapping(value = "/updateAllAcc",
+			consumes  = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public void updateAllAccount() { 
-    	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account("1367", "54000", 54));
-    	acclist.add( new Account("1368", "54000", 54));
-    	batchrepository.updateAllAccounts(acclist, 58);
+    public void updateAllAccount(@RequestBody List<Account> accList) { 
+
+		  batchrepository.updateAllAccounts(accList, 58);
 	}
     /**
      * To delete All customers with batch processing
      */
+    @Operation(summary = "Returns a greeting message")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(type = "object"))) })
     @GetMapping(value = "/deleteAllCust",
-			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
 			headers = "Accept=application/json")
-    public void deleteAllCust() { 
-    	List<Account> acclist = new ArrayList<>();
-    	acclist.add(new Account("1367", "54000", 54));
-    	acclist.add( new Account("1368", "54000", 54));
-    	List<Customer> custlist = new ArrayList<>();
-    	custlist.add(new Customer("1", "34", "abc", acclist));
-    	custlist.add(new Customer("2", "34", "abc", acclist));
-    	batchrepository.deleteAll(custlist, "sayantan");
+    public void deleteAllCust(@RequestBody List<Customer> custList) { 
+		  batchrepository.deleteAll(custList, "sayantan");
 	}
 }
